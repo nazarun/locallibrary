c4202537efdc8de54058ae1599baf08f92460185
Nazar <nazarun@gmail.com>
2018-01-16 15:32:06 +0200

    First version

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var genreSchema = new Schema({
+	name: {type: String, required: true, min: 3, max: 100},
+});
+
+//virtual for genre's url
+genreSchema.virtual("url").get(function(){
+	return "/catalog/genre/" + this._id;
+});
+
+//Export model
+module.exports = mongoose.model("Genre", genreSchema);
